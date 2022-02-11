378c6c84d0fda3fc29daa0c1825a3e8a8dac9c2d
Gimhan Ishara <bandarawggi.19@uom.lk>
2022-02-11 23:38:16 +0530

    develop the signIn page

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React, useState } from "react";
+import { StyleSheet, Text, View, Button } from 'react-native';
+import SignIn from "./components/signIn";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SignIn/>
     </View>
   );
 }
