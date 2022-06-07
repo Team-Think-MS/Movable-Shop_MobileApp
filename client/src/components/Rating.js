84bb9049afc4e8ea1e69018e7960ad04c1a62cdd
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2022-06-07 21:55:47 +0530

    styled rating stars

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -8,49 +8,50 @@ const Rating = ({rating,numReviews}) => {
     <View style={{flexDirection:'row',width:'100%',position:'relative',paddingHorizontal:15}}>
       <View >
          {rating >= 1
-                  ? <Icon name="star-outline" type='material-community' color="#65686e" size ={16}/>
+                  ? <Icon name="star" type='material-community'color="#65686e"size ={16}/>
                   : rating >= 0.5
                   ? <Icon name="star-half-full" type='material-community'color="#65686e"size ={16}/>
-                  : <Icon name="star" type='material-community'color="#65686e"size ={16}/>}  
+                  :   <Icon name="star-outline" type='material-community' color="#65686e" size ={16}/>
+                  }
   
       </View>
       <View>
        {
             rating >= 2
-              ?  <Icon name="star-outline" type='material-community'color="#65686e"size ={16}/>
+              ?  <Icon name="star" type='material-community'color="#65686e"size ={16}/>
               : rating >= 1.5
               ? <Icon name="star-half-full" type='material-community'color="#65686e"size ={16}/>
-              : <Icon name="star" type='material-community'color="#65686e"size ={16}/>
+              :  <Icon name="star-outline" type='material-community' color="#65686e" size ={16}/>
           }
        
       </View>
       <View>
   {
   rating >= 3
-              ?  <Icon name="star-outline" type='material-community'color="#65686e"size ={16}/>
+              ?  <Icon name="star" type='material-community'color="#65686e"size ={16}/>
               : rating >= 2.5
               ? <Icon name="star-half-full" type='material-community'color="#65686e"size ={16}/>
-              :<Icon name="star" type='material-community'color="#65686e"size ={16}/>
+              : <Icon name="star-outline" type='material-community' color="#65686e" size ={16}/>
           }
 
       </View>
       <View>
  {
             rating >= 4
-              ?  <Icon name="star-outline" type='material-community'color="#65686e"size ={16}/>
+              ?  <Icon name="star" type='material-community'color="#65686e"size ={16}/>
               : rating >= 3.5
               ?<Icon name="star-half-full" type='material-community'color="#65686e"size ={16}/>
-              : <Icon name="star" type='material-community'color="#65686e"size ={16}/>
+              : <Icon name="star-outline" type='material-community' color="#65686e" size ={16}/>
           }
      
       </View>
       <View>
  {
             rating >= 5
-              ?  <Icon name="star-outline" type='material-community'color="#65686e"size ={16}/>
+              ? <Icon name="star" type='material-community'color="#65686e"size ={16}/>
               : rating >= 4.5
               ?<Icon name="star-half-full" type='material-community'color="#65686e"size ={16}/>
-              : <Icon name="star" type='material-community'color="#65686e"size ={16}/>
+              : <Icon name="star-outline" type='material-community' color="#65686e" size ={16}/>
           }
 
       </View>
